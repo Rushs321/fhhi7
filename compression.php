d2f3ccb0c174bb7a070ef69bc516d0ed85159afb
Rushs321 <fyggh566@tutamail.com>
2025-01-09 14:08:11 +0530

    Update compression.php

--- compression.php
+++ compression.php
@@ -19,21 +19,34 @@ function process_image()
         $width = imagesx($inst);
         $height = imagesy($inst);
 
-        // Check if the height exceeds 16383, and if so, resize
-        if ($height > 16383) {
-            $new_height = 16383;
-            $new_width = (int)($width * $new_height / $height);
-            $inst = imagescale($inst, $new_width, $new_height);
+        // Define maximum dimensions
+        $maxWidth = 800;
+        $maxHeight = 16383;
+
+        // Calculate the new dimensions while maintaining the aspect ratio
+        if ($width > $height) {
+            if ($width > $maxWidth) {
+                $height = (int)($height * $maxWidth / $width);
+                $width = $maxWidth;
+            }
+        } else {
+            if ($height > $maxHeight) {
+                $width = (int)($width * $maxHeight / $height);
+                $height = $maxHeight;
+            }
         }
+
+        // Resize the image
+        $inst = imagescale($inst, $width, $height);
 
         $ctx["instances"] += ["image" => $inst];
 
         if ($origin_type == "image/png" || $origin_type == "image/gif") {
             $i_palette($inst);
-        };
+        }
         if ($greyscale) {
             $i_filter($inst, IMG_FILTER_GRAYSCALE);
-        };
+        }
 
         ob_start();
 
@@ -42,13 +55,13 @@ function process_image()
         ob_end_clean();
         $i_destroy($inst);
 
-        array_walk($headers, fn ($v, $k) => $set_header($k . ": " . $v));
+        array_walk($headers, fn($v, $k) => $set_header($k . ": " . $v));
 
         $size = strlen($converted_image);
         $set_header("content-length: " . $size);
         $set_header("content-type: image/" . $format);
         $set_header("x-original-size: " . $origin_size);
-        $set_header("x-bytes-saved: " . $origin_size - $size);
+        $set_header("x-bytes-saved: " . ($origin_size - $size));
 
         echo $converted_image;
         return $ctx;
